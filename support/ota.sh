c16b0b4a768ce7d1ae79de51c623d9a14c65fa2a
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-16 20:43:53 +0530

    OTA Init and dumpling OTA test

--- support/ota.sh
+++ support/ota.sh
@@ -12,17 +12,17 @@ fi
 
 cd $ROMDIR
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
-FILENAME=$(find out/target/product/$DEVICE/Nezuko*.zip | cut -d "/" -f 5)
-ID=$(md5sum out/target/product/$DEVICE/Nezuko*.zip | cut -d " " -f 1)
+FILENAME=$(find out/target/product/$DEVICE/NEZUKO*.zip | cut -d "/" -f 5)
+ID=$(md5sum out/target/product/$DEVICE/NEZUKO*.zip | cut -d " " -f 1)
 FILEHASH=$ID
-SIZE=$(wc -c out/target/product/$DEVICE/Nezuko*.zip | awk '{print $1}')
+SIZE=$(wc -c out/target/product/$DEVICE/NEZUKO*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/nezukoos/files/$DEVICE/$FILENAME/download"
 VERSION="11"
 DONATE_URL="https://nezukoos.github.io/Website/donations.html"
 WEBSITE_URL="https://nezukoos.github.io/Website"
 NEWS_URL="https:\/\/t.me\/NezukoOSUpdates"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
-printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > $ROMDIR/OTAbuilds/$DEVICE.json
+printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > $ROMDIR/OTA/builds/$DEVICE.json
 echo $ROMDIR/OTA/builds/$DEVICE.json file created
 
 BUILD_DATE=$(echo $FILENAME | cut -d "-" -f 3)

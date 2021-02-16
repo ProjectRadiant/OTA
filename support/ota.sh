4ebadf69dd16eb06f0c6ad0609563a84f16c9d87
Maitreya29 <maitreyapatni30@gmail.com>
2021-02-16 19:49:19 +0530

    Minor NezukoOS modifications

--- support/ota.sh
+++ support/ota.sh
@@ -23,9 +23,9 @@ ID=$(md5sum out/target/product/$DEVICE/Nezuko*.zip | cut -d " " -f 1)
 FILEHASH=$ID
 SIZE=$(wc -c out/target/product/$DEVICE/Nezuko*.zip | awk '{print $1}')
 URL="https://sourceforge.net/projects/nezukoos/files/$DEVICE/$FILENAME/download"
-VERSION="10"
+VERSION="11"
 DONATE_URL="https://nezukoos.github.io/Website/donations.html"
-WEBSITE_URL="https://nezukoos,github.io/Website"
-NEWS_URL="https:\/\/t.me\/nezukorom"
+WEBSITE_URL="https://nezukoos.github.io/Website"
+NEWS_URL="https:\/\/t.me\/NezukoOSUpdates"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > ~/OTA/builds/$DEVICE.json

a19aa0424659e51ac91756c95edb10f462d25968
MLZ94 <zugutoroberto@gmail.com>
2021-03-13 20:00:07 +0000

    improve ota script

--- README.md
+++ README.md
@@ -3,10 +3,10 @@ Do Read This Before Proceeding :-
 Clone the OTA repo in your ROM directory.
 To generate json and push ota,
 ```bash
-bash OTA/support/ota.sh <device_codename> <rom_folder> <maintainer_tgusername> <maintainer_url> <forum_url>
+bash OTA/support/ota.sh <device_codename> <rom_folder> <maintainer_tgusername> <maintainer_url> <file_name>
 
 e.g
-bash OTA/support/ota.sh dumpling Nezuko "Maitreya29" https://t.me/Maitreya29 https://forum.xda-developers.com/
+bash OTA/support/ota.sh dumpling Nezuko "Maitreya29" https://t.me/Maitreya29 NEZUKO_dumpling-11.0-20210303-1301-STABLE-OFFICIAL.zip
 ```
 NOTES:
 - ROM directory will look for *home/<os_username>/<rom_folder>*.
--- support/ota.sh
+++ support/ota.sh
@@ -2,7 +2,7 @@ DEVICE=$1
 ROMDIR=~/$2
 MAINTAINER=$3
 MAINTAINER_URL=$4
-FORUM_URL=$5
+FILENAME=$5
 
 # Ensures that mandatory parameters are entered
 if [ $# -lt 5 ]; then
@@ -12,15 +12,15 @@ fi
 
 cd $ROMDIR
 DATETIME=$(grep "org.pixelexperience.build_date_utc=" out/target/product/$DEVICE/system/build.prop | cut -d "=" -f 2)
-FILENAME=$(find out/target/product/$DEVICE/NEZUKO*.zip | cut -d "/" -f 5)
-ID=$(md5sum out/target/product/$DEVICE/NEZUKO*.zip | cut -d " " -f 1)
+ID=$(md5sum out/target/product/$DEVICE/$FILENAME | cut -d " " -f 1)
 FILEHASH=$ID
-SIZE=$(wc -c out/target/product/$DEVICE/NEZUKO*.zip | awk '{print $1}')
+SIZE=$(wc -c out/target/product/$DEVICE/$FILENAME | awk '{print $1}')
 URL="https://sourceforge.net/projects/nezukoos/files/$DEVICE/$FILENAME/download"
 VERSION="11"
 DONATE_URL="https://nezukoos.github.io/Website/donations.html"
 WEBSITE_URL="https://nezukoos.github.io/Website"
 NEWS_URL="https:\/\/t.me\/NezukoOSUpdates"
+FORUM_URL="https://forum.xda-developers.com/"
 JSON_FMT='{\n"error":false,\n"filename": %s,\n"datetime": %s,\n"size":%s, \n"url":"%s", \n"filehash":"%s", \n"version": "%s", \n"id": "%s",\n"donate_url": "%s",\n"website_url":"%s",\n"news_url":"%s",\n"maintainer":"%s",\n"maintainer_url":"%s",\n"forum_url":"%s"\n}'
 printf "$JSON_FMT" "$FILENAME" "$DATETIME" "$SIZE" "$URL" "$FILEHASH" "$VERSION" "$ID" "$DONATE_URL" "$WEBSITE_URL" "$NEWS_URL" "$MAINTAINER" "$MAINTAINER_URL" "$FORUM_URL" > $ROMDIR/OTA/builds/$DEVICE.json
 echo $ROMDIR/OTA/builds/$DEVICE.json file created
